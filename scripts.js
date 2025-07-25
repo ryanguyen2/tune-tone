d2df14a08804af4e3af77044a5e9eac45fb85916
ryanguyen2 <rnguyen2102@gmail.com>
2025-07-25 17:36:19 -0400

    increased logo sizing to reduce blur

--- scripts.js
+++ scripts.js
@@ -1,27 +1,10 @@
-//dark mode function
-const darkModeSwitch = document.getElementById("checkbox");
-const currentMode = localStorage.getItem("mode") ? localStorage.getItem("mode") : null;
-const projectImg = document.querySelectorAll(".project-card-image img");
+//switch theme function
+const switchTheme = () => {
+    const rootElement = document.documentElement;
+    let dataTheme = rootElement.getAttribute('data-theme'); // FIXED typo
+    let newTheme = (dataTheme === 'light') ? 'dark' : 'light';
 
-if (currentMode) {
-    document.documentElement.setAttribute("mode", currentMode);
+    rootElement.setAttribute('data-theme', newTheme);
+};
 
-    if (currentMode === "dark") {
-        darkModeSwitch.checked = true;
-        projectImg.forEach(a => a.style.filter = "brightness(0.85)");
-    }
-}
-
-function switchMode(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute("mode", "dark");
-        localStorage.setItem("mode", "dark");
-        projectImg.forEach(a => a.style.filter = "brightness(0.85)");
-    } else {
-        document.documentElement.setAttribute("mode", "light");
-        localStorage.setItem("mode", "light");
-        projectImg.forEach(a => a.style.filter = "brightness(1)");
-    }
-}
-
-darkModeSwitch.addEventListener("change", switchMode, false);
+document.querySelector('#mode_switch').addEventListener('click', switchTheme); // FIXED selector
